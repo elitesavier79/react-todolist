9661982f97431b8ee0bbe9659393eac5c001b3ef
Dicky Swardi <elitesavier79@gmail.com>
2020-05-10 11:53:33 +0700

    migrating Html dan css ke react
    belajar react

--- src/App.js
+++ src/App.js
@@ -3,9 +3,41 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="kerangka">
+        <section className="header">
+          <button className="header-btn main-black-color">Add</button>
+          <h1 className="header-title">Todo Lits</h1>
+          <button className="header-btn main-red-color ">Clear</button>
+        </section>
+
+        <section>
+          <form className="add">
+            <div className="add-form">
+              <input type="text" className="add-input" />
+              <button className="add-btn main-black-color">Add</button>
+            </div>
+          </form>
+        </section>
+
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">learning React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
