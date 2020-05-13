4b8ec86ed40e84d00b04d0dd6587a10b514e29c2
Dicky Swardi <elitesavier79@gmail.com>
2020-05-13 12:45:04 +0700

    memodikasi todoform
    materi react course

--- src/components/todoform.jsx
+++ src/components/todoform.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import propType from "prop-types";
+import PropTypes from "prop-types";
 
-const Todoform = ({addTodo}) => {
+const Todoform = ({ addTodo }) => {
   const [value, setValue] = useState("");
 
   const handleFormSubmit = e => {
     e.preventDefault();
 
     if (!value) {
-     alert("no blank todo!");
-     return;
+      alert("no blank todo!");
+      return;
     }
 
     addTodo(value);
@@ -18,24 +18,22 @@ const Todoform = ({addTodo}) => {
   };
 
   return (
-    <section>
-      <form className="add">
-        <div className="add-form" onSubmit={handleFormSubmit}>
-          <input
-            type="text"
-            className="add-input"
-            value={value}
-            onChange={e => setValue(e.target.value)}
-          />
-          <button className="add-btn main-black-color">Add</button>
-        </div>
+    <section className="add">
+      <form className="add-form" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        <button className="add-btn main-black-color">Add</button>
       </form>
     </section>
   );
 };
 
-Todoform.propType {
-  addTodo: propType.func.isRequired
+Todoform.propTypes = {
+  addTodo: PropTypes.func.isRequired
 };
 
 export default Todoform;
