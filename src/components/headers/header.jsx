0aa4832a897d4d3dafa2c1ece5329d95e79da13e
Dicky Swardi <elitesavier79@gmail.com>
2020-05-26 15:23:00 +0700

    membuat emotion css props untuk header component
    materi react course

--- src/components/headers/header.jsx
+++ src/components/headers/header.jsx
@@ -1,16 +1,24 @@
 /** @jsx jsx */
+
 //import React from "react";
 import { jsx } from "@emotion/core";
 import PropTypes from "prop-types";
 import Button from "../button/button";
 
-import styles from "./header.module.css";
+//import styles from './header.module.css';
+import * as styles from "./header.styles";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
   return (
-    <section className={styles.header}>
+    //<section className={styles.header}>
+    //<Button text={showAdd ? 'FINISH' : 'ADD'} onClick={showAddToggle} />
+    //<h1 className={styles.headerTitle}>Todo Lits</h1>
+    //<Button text="CLEAR" onClick={clearTodos} color="red" align="right"/>
+    //</section>
+
+    <section css={styles.header}>
       <Button text={showAdd ? "FINISH" : "ADD"} onClick={showAddToggle} />
-      <h1 className={styles.headerTitle}>Todo Lits</h1>
+      <h1 css={styles.headerTitle}>Todo Lits</h1>
       <Button text="CLEAR" onClick={clearTodos} color="red" align="right" />
     </section>
   );
