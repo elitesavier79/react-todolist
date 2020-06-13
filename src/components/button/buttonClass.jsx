d921eb7c8dcc7d9eef34b233c78e0ea73389378c
Dicky Swardi <elitesavier79@gmail.com>
2020-06-13 22:06:16 +0700

    membuat class jsx pada header component
    materi react course

--- src/components/button/buttonClass.jsx
+++ src/components/button/buttonClass.jsx
@@ -1,5 +1,5 @@
-import react from "react";
-import styled from "@emotion/styled";
+import React from "react";
+import Styled from "@emotion/styled";
 import PropTypes from "prop-types";
 import { withTheme } from "emotion-theming";
 
@@ -55,4 +55,4 @@ class Button extends React.Component {
   };
 }
 
-export default withtheme(Button);
+export default withTheme(Button);
--- src/pages/todolist.jsx
+++ src/pages/todolist.jsx
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 //import Paper from "../components/paper/paper";
 import Paper from "../components/paper/paperClass";
 //import Header from "../components/headers/header";
-import Header from "../components/headers/headerNoJSX";
+//import Header from "../components/headers/headerNoJSX";
+import Header from "../components/headers/headerClass";
+
 import Todoform from "../components/todoform/todoform";
 import Todos from "../components/todos/todos";
 
