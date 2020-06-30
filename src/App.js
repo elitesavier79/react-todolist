3a9a81504b7690b121e88f259bc0a65401ecfd34
Dicky Swardi <elitesavier79@gmail.com>
2020-06-30 12:19:16 +0700

    membuat efek hook local storage di todilist component matari react course

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./styles.css";
 import { ThemeProvider } from "emotion-theming";
 
-//import Todolist from "./pages/todolist";
-import Todolist from "./pages/todolistClass";
+import Todolist from "./pages/todolist";
+//import Todolist from "./pages/todolistClass";
 
 const theme = {
   color: {
--- src/pages/todolist.jsx
+++ src/pages/todolist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //import Paper from "../components/paper/paper";
 import Paper from "../components/paper/paperClass";
@@ -14,13 +14,21 @@ import Todos from "../components/todos/todosClass";
 import Container from "../layout/container";
 
 const Todolist = () => {
-  const [todos, setTodos] = useState([
-    { text: "leaning react", isCompleted: false },
-    { text: "leaning react hook", isCompleted: false },
-    { text: "leaning styling in react", isCompleted: false }
-  ]);
+  //const [todos, setTodos] = useState([
+  //{ text: "leaning react", isCompleted: false },
+  //{ text: "leaning react hook", isCompleted: false },
+  // { text: "leaning styling in react", isCompleted: false }
+  //]);
+
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
 
   const [showAdd, setShowAdd] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = value => {
     if (todos.length < 8) {
